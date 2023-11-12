3958c38e0426d1f73145e15c1e8a7af3536f8282
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-12 07:52:26 +0300

    Update 9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+def print_last_digit(number):
+    # Calculate the last digit of the number using the modulo operator (%)
+    last_digit = abs(number) % 10
+
+    # Print the last digit without a newline (using "end=")
+    print(last_digit, end="")
+
+    # Return the value of the last digit
+    return last_digit
