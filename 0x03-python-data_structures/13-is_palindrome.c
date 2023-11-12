92900d7bf950b9e103a73b29c45b9aefd97479a4
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-12 14:14:37 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,40 @@
 int is_palindrome(listint_t **head) {
-    listint_t *slow = *head;  /* Initialize slow pointer at the head of the list */
-    listint_t *fast = *head;  /* Initialize fast pointer at the head of the list */
-    listint_t *prev = NULL;    /* Initialize a pointer to reverse the first half of the list */
-    listint_t *temp;           /* Temporary pointer */
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL; /*pointer to reverse the first half of the list */
+	listint_t *temp;           /* Temporary pointer */
 
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        return (1);  /* An empty list or a list with only one element is a palindrome */
-    }
+    /* An empty list or a list with only one element is a palindrome */
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (1);
+	}
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        /* Move fast two steps and slow one step */
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		/* Move fast two steps and slow one step */
+		fast = fast->next->next;
 
-        /* Reverse the first half of the list */
-        temp = slow;
-        slow = slow->next;
-        temp->next = prev;
-        prev = temp;
-    }
+		/* Reverse the first half of the list */
+		temp = slow;
+		slow = slow->next;
+		temp->next = prev;
+		prev = temp;
+	}
+    /* If the list has an odd number of elements, adjust slow */
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
 
-    if (fast != NULL)
-    {
-        slow = slow->next;  /* If the list has an odd number of elements, adjust slow */
-    }
+	while (prev != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);  /* It's not a palindrome */
 
-    while (prev != NULL)
-    {
-        if (prev->n != slow->n) {
-            return (0);  /* It's not a palindrome */
-        }
-        prev = prev->next;
-        slow = slow->next;
-    }
+		prev = prev->next;
+		slow = slow->next;
+	}
 
-    return (1);  /* It's a palindrome */
+	return (1);  /* It's a palindrome */
 }
