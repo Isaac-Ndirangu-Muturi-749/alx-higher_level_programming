fe50629a6c00ea8731189c059de4ecd51394f64a
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-12 11:12:08 +0300

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-        # If tuple_a or tuple_b is smaller than 2 elements, append 0s to make them
+    # If tuple_a or tuple_b is smaller than 2 elements, append 0s to make them
     # both have 2 elements using the += operator.
     tuple_a += (0, 0)
     tuple_b += (0, 0)
