29dcea425d193f9acf3afe6f2a3b13d3f3382505
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-23 01:18:25 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,8 +5,8 @@ class Square:
     """
     This is the Square class.
 
-    A square is a geometric shape with four equal sides and four equal angles.
-    It is defined by its side length.
+    A square is a geometric shape with four equal sides and
+    four equal angles. It is defined by its side length.
 
     Attributes:
     - None
