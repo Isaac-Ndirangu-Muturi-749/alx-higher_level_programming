328b1bcc847007dc95beadef00d772ea1881fe92
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-23 08:01:57 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 
+"""This module defines a Square class."""
+
 class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size=0):
         """Initialize a square with a given size.
 
         Args:
             size (int): The size of the square. Default is 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         self.__size = size
 
