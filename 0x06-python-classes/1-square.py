89a0b484e6233c8f4fa422f2e8cd3635aa00ac43
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-23 07:06:54 +0300

    Update 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    This is the Square class.
+
+    A square is a geometric shape with four equal sides and four equal angles.
+    It is defined by its side length.
+
+    Attributes:
+    - __size (int): Private instance attribute representing the size of the square.
+
+    Methods:
+    - __init__(self, size): Initializes an instance of the Square class with a given size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes an instance of the Square class with a given size.
+
+        Parameters:
+        - size (int): The size of the square.
+
+        Raises:
+        - TypeError: If the provided size is not an integer.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
