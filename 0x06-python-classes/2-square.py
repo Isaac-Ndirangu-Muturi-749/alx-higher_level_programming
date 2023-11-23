390b3b73eea5a6da38d9cc84723d3e781a79980c
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-23 07:55:58 +0300

    Update 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        """Initialize a square with a given size.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
