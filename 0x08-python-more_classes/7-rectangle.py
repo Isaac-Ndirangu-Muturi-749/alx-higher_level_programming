f83fce56c583d6004b78c00e79196ff39fb5b979
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-28 01:08:24 +0300

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -132,7 +132,9 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        lines = [str(self.symbol) * self.__width for _ in range(self.__height)]
+        result = '\n'.join(lines)
+        return result
 
     def __repr__(self):
         """
