ab2962f8ca4d79e79ed20144eda0c542295e1b25
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-28 01:20:20 +0300

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -151,7 +151,8 @@ class Rectangle:
             TypeError: If rect_1 or rect_2 is not an instance of Rectangle.
 
         Returns:
-            Rectangle: The rectangle with the larger area, or rect_1 if both have the same area.
+            Rectangle: The rectangle with the larger area, or rect_1 if both
+             have the same area.
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
