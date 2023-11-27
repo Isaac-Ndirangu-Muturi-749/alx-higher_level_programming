0ffbc6339ec6cc0a0651b6f9aa8f6966a2fed897
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-27 23:28:16 +0300

    Update 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+"""
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+
+    Attributes:
+        None.
+
+    Methods:
+        None.
+    """
+
+    pass
