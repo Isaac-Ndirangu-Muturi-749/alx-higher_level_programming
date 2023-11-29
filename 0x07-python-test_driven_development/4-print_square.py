5dd36ecd6c9ac642860bf69c659a1070cbe33839
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-29 05:39:06 +0300

    task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,7 @@
 This module defines the print_square function.
 """
 
+
 def print_square(size):
     """
     Prints a square with the character #.
@@ -13,31 +14,6 @@ def print_square(size):
     Raises:
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
-
-    Example:
-    >>> print_square(4)
-    ####
-    ####
-    ####
-    ####
-    >>> print_square(10)
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    >>> print_square(0)
-    >>> print_square(1)
-    #
-    >>> print_square(-1)
-    Traceback (most recent call last):
-        ...
-    ValueError: size must be >= 0
     """
 
     if not isinstance(size, int):
@@ -48,18 +24,3 @@ def print_square(size):
 
     for _ in range(size):
         print("#" * size)
-
-if __name__ == "__main__":
-    print_square(4)
-    print("")
-    print_square(10)
-    print("")
-    print_square(0)
-    print("")
-    print_square(1)
-    print("")
-    try:
-        print_square(-1)
-    except Exception as e:
-        print(e)
-    print("")
