e07c02acfcdab8eaa79976df675770714632a02b
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-29 06:25:53 +0300

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,7 @@
 This module defines the text_indentation function.
 """
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
@@ -12,54 +13,33 @@ def text_indentation(text):
 
     Raises:
         TypeError: If text is not a string.
-
-    Example:
-    >>> text_indentation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-    Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-    Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-    rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-    stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-    cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-    beatiorem! Iam ruinas videres")
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Quonam modo?
-    Utrum igitur tibi litteram videor an totas paginas commovere?
-    Non autem hoc:
-    igitur ne illud quidem.
-    Fortasse id optimum, sed ubi illud:
-    Plus semper voluptatis?
-    Teneo, inquit, finem illi videri nihil dolere.
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum rationi oboediens.
-    Si id dicis, vicimus.
-    Inde sermone vario sex illa a Dipylo stadia confecimus.
-    Sin aliud quid voles, postea.
-    Quae animi affectio suum cuique tribuens atque hanc, quam dico.
-    Utinam quidem dicerent alium alio beatiorem! Iam ruinas videres
-
     """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    chars = ".?:"
-    start = 0
+    indentation_chars = ".?:"
+    index = 0
 
-    for i, char in enumerate(text):
-        if char in chars:
-            print(text[start:i + 1].strip())
-            print()
-            start = i + 1
-    print(text[start:].strip())
+    # Skip leading spaces
+    while index < len(text) and text[index] == ' ':
+        index += 1
 
-if __name__ == "__main__":
-    text_indentation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-    Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-    Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-    rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-    stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-    cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-    beatiorem! Iam ruinas videres")
+    while index < len(text):
+        # Print the current character without a newline
+        print(text[index], end="")
+
+        # Check for newline or indentation characters
+        if text[index] == "\n" or text[index] in indentation_chars:
+            if text[index] in indentation_chars:
+                # Print two new lines after encountering ., ? or :
+                print("\n")
+
+            # Skip consecutive spaces after newline or indentation characters
+            index += 1
+            while index < len(text) and text[index] == ' ':
+                index += 1
+            continue
+
+        # Move to the next character
+        index += 1
