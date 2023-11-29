0225805baf84679f3c2b86300f063a958a51fc5f
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-29 05:32:25 +0300

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 This module defines the say_my_name function.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first name> <last name>".
@@ -13,14 +14,6 @@ def say_my_name(first_name, last_name=""):
 
     Raises:
         TypeError: If first_name or last_name is not a string.
-
-    Example:
-    >>> say_my_name("John", "Smith")
-    My name is John Smith
-    >>> say_my_name("Walter", "White")
-    My name is Walter White
-    >>> say_my_name("Bob")
-    My name is Bob
     """
 
     if not isinstance(first_name, str):
@@ -30,8 +23,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
-
-if __name__ == "__main__":
-    say_my_name("John", "Smith")
-    say_my_name("Walter", "White")
-    say_my_name("Bob")
