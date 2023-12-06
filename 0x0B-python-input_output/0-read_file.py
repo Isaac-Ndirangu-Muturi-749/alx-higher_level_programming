92451a15938dc2f9939a496cfd09e2a9da9aa5f2
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-06 11:22:45 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """Defines a text file-reading function."""
 
-def read_file(filename=""):
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end='')
-
 
 def read_file(filename=""):
     """
