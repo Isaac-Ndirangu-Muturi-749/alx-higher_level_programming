0761742bd3c3b617aaf1e04977ff86ff3d7b6ca2
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-06 13:20:31 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,6 +2,7 @@
 """Defines a function that returns the dictionary description with
  simple data structure for JSON serialization of an object."""
 
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
     for JSON serialization of an object.
