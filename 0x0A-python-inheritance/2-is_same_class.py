6fbddeaa5f9728093ee76a6a2cba08141fbc33ec
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-06 16:56:00 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,6 +2,7 @@
 """Defines a function that checks if an object is an instance of
 a specified class."""
 
+
 def is_same_class(obj, a_class):
     """Check if obj is exactly an instance of a_class."""
     return type(obj) is a_class
