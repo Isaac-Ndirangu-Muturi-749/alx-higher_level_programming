b4bc0251c14dd327ba1fb987f0ed424bb9d89211
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-06 11:45:25 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,15 @@ from os import path
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
-
 filename = "add_item.json"
 
-# Check if the file exists
-if not path.exists(filename):
-    save_to_json_file([], filename)
 
-# Load the existing list from the file
-my_list = load_from_json_file(filename)
+try:
+    # Load the existing list from the file
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    # If the file doesn't exist, create an empty list
+    my_list = []
 
 # Add command line arguments to the list
 my_list.extend(sys.argv[1:])
