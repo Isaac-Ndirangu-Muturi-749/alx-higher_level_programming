a1a3a28335d148ca5c6231930650c335dc48e983
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-09 13:18:21 +0300

    task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -44,3 +44,7 @@ class Square(Rectangle):
         """Return a string representation of the Square."""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
