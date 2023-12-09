bd5ce72f5587fae92ab740770a8d95910a506fe3
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-09 13:21:16 +0300

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Defines the Base class."""
 
+import json
+
 
 class Base:
-    """Base class for managing id attribute in other classes."""
+    """Base class for other classes in the project."""
 
     __nb_objects = 0
 
@@ -11,12 +13,25 @@ class Base:
         """Initialize a new instance of Base.
 
         Args:
-            id (int): If provided, assigns the value to the public
-                instance attribute id. Otherwise, increments __nb_objects
-                and assigns the new value to id.
+            id (int): ID of the instance. If None, it will be assigned by
+                the Base class.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
