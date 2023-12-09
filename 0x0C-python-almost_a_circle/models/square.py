235877b7c2f115f6c21749ae7dfe77c85ae325ca
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-12-09 13:07:49 +0300

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,6 +19,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the Square."""
         return "[Square] ({}) {}/{} - {}".format(
