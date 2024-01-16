d861fcd6b891e0c699334c025c6554bd81863fbb
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-01-16 22:09:42 +0300

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -3,6 +3,5 @@
 -- id = 89
 -- name = Best School
 -- The database name will be passed as an argument of the mysql command
-INSERT INTO hbtn_0c_0.first_table (id, name)
-VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
 
