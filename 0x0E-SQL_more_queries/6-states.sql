e26369238c947dd14d97835a61322a8d589cabd8
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-01-18 01:11:08 +0300

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,6 +6,7 @@
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
