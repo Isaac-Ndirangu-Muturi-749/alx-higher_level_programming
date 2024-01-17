c7a441b5a111fc8012f654a1b866bc4a6011edfd
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-01-18 00:35:28 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,9 @@
--- Write a script that creates the MySQL server user user_0d_1.
--- user_0d_1 should have all privileges on your MySQL server
--- The user_0d_1 password should be set to user_0d_1_pwd
--- If the user user_0d_1 already exists, your script should not fail
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+ GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+ FLUSH PRIVILEGES;
