25d3064433dd4b0fa380a6d40deb0384a59c8864
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-13 17:16:41 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 // This script prints three lines of text
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
