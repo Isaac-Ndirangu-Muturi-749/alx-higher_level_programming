2a35bfdb6a5753b06f182de76a150e8f9fd81c01
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-14 19:12:13 +0300

    update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,19 +4,14 @@
 const x = process.argv[2];
 
 // Check if x is a valid number
-if (isNaN(x)) {
+if (x === undefined || isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   // Convert x to an integer
   const numOccurrences = parseInt(x);
-
-  // Check if numOccurrences is positive
-  if (numOccurrences > 0) {
-    // Loop to print "C is fun" numOccurrences times
-    for (let i = 0; i < numOccurrences; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
+  let i = 0;
+  while (i < numOccurrences) {
+    console.log('C is fun');
+    i++;
   }
 }
