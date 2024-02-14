4d6e84ee31e4a5453fc306781c47e8a6d85c9ec8
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-14 19:01:55 +0300

    update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      // Create an empty object if width or height is not a
-      // positive integer or equal to 0
-      return {};
-    } else {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
