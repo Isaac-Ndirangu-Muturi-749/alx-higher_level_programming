d8bee3e4b291d953d8fbd0fa9abb6280cecad68d
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-17 15:04:05 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute SQL query to select states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch all the rows
     rows = cursor.fetchall()
