b2c2d20ccbb3e8597027a3d8536bd0879c90df84
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-02-17 15:15:39 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        sys.exit(1)
-
     # Extracting arguments
     username = sys.argv[1]
     password = sys.argv[2]
@@ -17,22 +12,17 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
 
-        # Execute SQL query
-        cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name,))
+    # Execute SQL query
+    cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (state_name,))
 
-        # Fetch all rows and display results
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+    # Fetch all rows and display results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-        # Close cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
