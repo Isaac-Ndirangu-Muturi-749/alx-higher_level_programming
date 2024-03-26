d077d4ddf4945ccd9626a39559fa03a8d0429794
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-26 15:13:31 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const request = require('request');
 const url = process.argv[2];
 
 // Make a GET request to the URL
-request.get(process.argv[2]).on('response', function (response) {
-	console.log(`code: ${response.statusCode}`);
-  });
+request.get(url).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
